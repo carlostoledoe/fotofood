b4a4194490ec5c3e4cbb0246816b53e3167d94a7
Carlos Toledo <carlombo@hotmail.com>
2022-10-18 18:47:15 -0300

    Config form and controller for Cocoon

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
   has_many :tags, dependent: :destroy
-  accepts_nested_attributes_for :tags  
+  accepts_nested_attributes_for :tags, reject_if: ->(attrs) { attrs['name'].blank? }
 end
