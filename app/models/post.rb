e80f2e137f4e09bd2e5b3e20c4771eed60f4b63a
Carlos Toledo <carlombo@hotmail.com>
2022-10-19 20:00:18 -0300

    Se agrega dependencias user - post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,5 @@
 class Post < ApplicationRecord
   has_many :tags, dependent: :destroy
+  belongs_to :user
   accepts_nested_attributes_for :tags, reject_if: ->(attrs) { attrs['name'].blank? }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_19_224007) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_19_225614) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_19_224007) do
     t.string "place"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -41,5 +43,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_19_224007) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
   add_foreign_key "tags", "posts"
 end
