9c81606235025de4d6a3f8be241768bb74f25dcd
Carlos Toledo <carlombo@hotmail.com>
2022-10-20 14:27:01 -0300

    Se soluciona problema con el logout (rutas)

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  resources :users do
-    resources :posts, only: [:index, :show]
-  end
-
+  
   devise_for :users
   resources :tags
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  
   # Defines the root path route ("/")
+  resources :users do
+    resources :posts, only: [:index, :show]
+  end
   root "posts#index"
 end
